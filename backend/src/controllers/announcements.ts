80a1d72ac2959b308344a74e3f93c059bf69ea4e
Samuel Dolezal <samko.dolezal@gmail.com>
2026-05-23 11:58:17 +0200

    feat(backend): did all CRUD Routes Service Controller Routes

--- backend/src/controllers/announcements.ts
+++ backend/src/controllers/announcements.ts
@@ -1,26 +1,64 @@
 import type { Request, Response } from "express";
-import { listAnnouncements } from "../services/announcements.js";
+import {
+  createAnnouncement,
+  getAnnouncement,
+  listAnnouncements,
+  removeAnnouncement,
+  updateAnnouncement,
+} from "../services/announcements.js";
 import { Category } from "../generated/prisma/enums.js";
+import { ValidationError } from "../lib/errors.js";
 
-export async function list(req: Request, res: Response) {
+const getId = (req: Request) => {
+  const id = req.params.id;
+  if (typeof id !== "string") {
+    throw new ValidationError("Missing id");
+  }
+  return id;
+};
+const getBody = (req: Request) => {
+  const { title, body, categories, publishedAt } = req.body;
+  if (!title || !body || !categories || !publishedAt) {
+    throw new ValidationError(
+      "One of the requirements for request was not found.",
+    );
+  }
+  return {
+    title,
+    body,
+    categories,
+    publishedAt: new Date(publishedAt),
+  };
+};
+
+export const list = async (req: Request, res: Response) => {
   const category = req.query.category as Category | undefined;
   const search = req.query.search as string | undefined;
-  const list = await listAnnouncements(category, search);
-  res.json({ list: list });
-}
+  const announcements = await listAnnouncements(category, search);
+  res.json(announcements);
+};
 
-export async function getById(req: Request, res: Response) {
-  res.json({ todo: "detail", id: req.params.id });
-}
+export const getById = async (req: Request, res: Response) => {
+  const id = getId(req);
+  const announcement = await getAnnouncement(id);
+  res.json(announcement);
+};
 
-export async function create(req: Request, res: Response) {
-  res.status(201).json({ todo: "create", body: req.body });
-}
+export const create = async (req: Request, res: Response) => {
+  const body = getBody(req);
+  const newAnnouncement = await createAnnouncement(body);
+  res.status(201).json(newAnnouncement);
+};
 
-export async function update(req: Request, res: Response) {
-  res.json({ todo: "update", id: req.params.id, body: req.body });
-}
+export const update = async (req: Request, res: Response) => {
+  const id = getId(req);
+  const body = getBody(req);
+  const updatedAnnouncement = await updateAnnouncement(id, body);
+  res.json(updatedAnnouncement);
+};
 
-export async function remove(req: Request, res: Response) {
+export const remove = async (req: Request, res: Response) => {
+  const id = getId(req);
+  await removeAnnouncement(id);
   res.status(204).send();
-}
+};
--- backend/src/services/announcements.ts
+++ backend/src/services/announcements.ts
@@ -1,18 +1,30 @@
 import { prisma } from "../lib/db.js";
-import { Category } from "../generated/prisma/enums.js";
 import { Prisma } from "../generated/prisma/client.js";
+import { Category } from "../generated/prisma/enums.js";
+import { NotFoundError } from "../lib/errors.js";
 
-export const listAnnouncements = async (
-  category?: Category,
-  search?: string,
-) => {
+type CreateAnnouncementData = {
+  title: string;
+  body: string;
+  categories: Category[];
+  publishedAt: Date;
+};
+
+const findAnnouncement = async (id: string) => {
+  const announcement = await prisma.announcement.findUnique({ where: { id } });
+  if (!announcement) {
+    throw new NotFoundError("Announcement was not found.");
+  }
+  return announcement;
+};
+
+export const listAnnouncements = (category?: Category, search?: string) => {
   const where: Prisma.AnnouncementWhereInput = {};
 
   if (category) {
-    where.categories = {
-      has: category,
-    };
+    where.categories = { has: category };
   }
+
   if (search) {
     where.OR = [
       { title: { contains: search, mode: "insensitive" } },
@@ -20,9 +32,29 @@ export const listAnnouncements = async (
     ];
   }
 
-  const list = await prisma.announcement.findMany({
-    where: where,
+  return prisma.announcement.findMany({
+    where,
     orderBy: { updatedAt: "desc" },
   });
-  return list;
+};
+
+export const getAnnouncement = async (id: string) => {
+  return findAnnouncement(id);
+};
+
+export const createAnnouncement = (data: CreateAnnouncementData) => {
+  return prisma.announcement.create({ data });
+};
+
+export const updateAnnouncement = async (
+  id: string,
+  data: CreateAnnouncementData,
+) => {
+  await findAnnouncement(id);
+  return prisma.announcement.update({ where: { id }, data });
+};
+
+export const removeAnnouncement = async (id: string) => {
+  await findAnnouncement(id);
+  return prisma.announcement.delete({ where: { id } });
 };
